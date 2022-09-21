9bc316229a248ff090c24322de1e089279356b16
axumrs <rolson867@gmail.com>
2022-09-21 15:02:32 +0800

    impl CategoryService

--- category-srv/src/main.rs
+++ category-srv/src/main.rs
@@ -1,4 +1,15 @@
+use blog_proto::category_service_server::CategoryServiceServer;
+
 mod server;
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let addr = "127.0.0.1:19527";
+    println!("category-srv run at: {}", addr);
+
+    let category_srv = server::Category::new();
+    tonic::transport::Server::builder()
+        .add_service(CategoryServiceServer::new(category_srv))
+        .serve(addr.parse().unwrap())
+        .await
+        .unwrap();
 }
